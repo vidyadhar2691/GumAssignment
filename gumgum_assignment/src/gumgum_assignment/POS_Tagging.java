5babf351cfae888564b4465fd81705d26f7130ad
vidyadhar2691 <vidyadhar.2691@gmail.com>
2016-10-06 15:33:54 -0700

    Initial Commit

---
+++ gumgum_assignment/src/gumgum_assignment/POS_Tagging.java
@@ -0,0 +1,48 @@
+package gumgum_assignment;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+
+public class POS_Tagging {
+
+	public static void main(String[] args){
+		// TODO Auto-generated method stub
+		try {
+		//Arraylist to store URls
+		ArrayList<URL> webpages= new ArrayList<URL>();
+		webpages.add(new URL("http://www.gumgum.com/"));
+		webpages.add(new URL("http://www.popcrunch.com/jimmy-kimmel-engaged/"));
+		webpages.add(new URL("http://gumgum-public.s3.amazonaws.com/numbers.html"));
+		webpages.add(new URL("http://www.windingroad.com/articles/reviews/quick-drive-2012-bmw-z4-sdrive28i/"));
+		
+		//PageTagger object creation
+		PageTagger obj = new PageTagger();
+		PrintStream output;
+		//
+		for (int i=0;i<webpages.size();i++){
+			String a=obj.getText(webpages.get(i)); //Extracting data from the webpages.
+			//Creating output files for each URL(webpage).
+			output = new PrintStream(new FileOutputStream("URL_"+Integer.toString(i+1)+"_output.txt"));
+			//Tagging the extracted data.
+			output.print(obj.tagText(a));
+			output.close();
+			}			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			System.out.println(e);
+		} catch (MalformedURLException e1) {
+			// TODO Auto-generated catch block
+			System.out.println(e1);
+		} 
+		
+		System.out.println("Done...Please refresh the project to view the output files.");
+		
+		
+
+	}
+
+}
---
+++ gumgum_assignment/src/gumgum_assignment/PageTagger.java
@@ -0,0 +1,45 @@
+package gumgum_assignment;
+
+import java.io.IOException;
+import java.net.URL;
+
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import edu.stanford.nlp.tagger.maxent.MaxentTagger;
+
+public class PageTagger {
+	//Initiating MaxentTagger
+	MaxentTagger tag = new MaxentTagger("taggers/english-left3words-distsim.tagger");
+	public String tagText(String text){		
+				
+		//Splitting data into array to avoid java.lang.OutOfMemoryError: GC overhead limit exceeded
+		String[] new_text=text.split(" ");
+		String tagged_text="";
+		for (int i=0; i<new_text.length;i++){
+			tagged_text=tagged_text.concat(tag.tagString(new_text[i]));
+		}		
+		return tagged_text;
+	}
+	
+	public String getText(URL url){
+		
+		String data ="";
+		try {
+			//Parsing data from webpage
+			Connection conn= Jsoup.connect(url.toString()).maxBodySize(0);
+			Document doc=Jsoup.parse(conn.get().html());
+			data =doc.body().text();
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			System.out.println(e);;
+		}
+		return data.trim();
+		
+		
+		
+		
+	}
+
+}
